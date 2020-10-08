3c143da03e35b0132ef0aea8ce6f66840ab59996
Simon Parker <sparker@pivotal.io>
2020-10-08 09:08:48 -0400

    Tests are passing, but our solution doesn't quite match the Good Uncle's

--- java/src/main/java/com/bowling/bowling/Game.java
+++ java/src/main/java/com/bowling/bowling/Game.java
@@ -1,13 +1,27 @@
 package com.bowling.bowling;
 
 public class Game {
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currRollIndex = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currRollIndex++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < currRollIndex; i += 2) {
+            score += isSpare(i)
+                    ? 2 * rolls[i]
+                    : rolls[i];
+            if ((i + 1) < rolls.length) score += rolls[i + 1];
+
+        }
         return score;
     }
+
+    boolean isSpare(int indexAtStartOfFrame) {
+        return (indexAtStartOfFrame > 1 && rolls[indexAtStartOfFrame - 2] + rolls[indexAtStartOfFrame - 1] == 10);
+    }
+
 }
--- java/src/test/java/com/bowling/bowling/BowlingGameTest.java
+++ java/src/test/java/com/bowling/bowling/BowlingGameTest.java
@@ -35,8 +35,9 @@ public class BowlingGameTest {
     void testOneSpare() {
         g.roll(5);
         g.roll(5); // spare
-        g.roll(3);
+        g.roll(5);
+        g.roll(1);
         rollMany(17, 0);
-        assertEquals(16, g.score());
+        assertEquals(21, g.score());
     }
 }
